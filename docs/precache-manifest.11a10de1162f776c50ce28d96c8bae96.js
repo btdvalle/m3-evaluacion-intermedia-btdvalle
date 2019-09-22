dbeb19a0793dd3c425607f6c34ef8d31940c9931
btdvalle <btdvalle@gmail.com>
2019-09-22 11:44:15 +0200

    Update docs

---
+++ docs/precache-manifest.11a10de1162f776c50ce28d96c8bae96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296df914503d54724915ab046d6796d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5044c006737cc716f16",
+    "url": "./static/css/main.0ee25da8.chunk.css"
+  },
+  {
+    "revision": "98f4ebc6abc2cb9a0e7d",
+    "url": "./static/js/2.b46d8063.chunk.js"
+  },
+  {
+    "revision": "d5044c006737cc716f16",
+    "url": "./static/js/main.19823b84.chunk.js"
+  },
+  {
+    "revision": "68d377a751b5275e84d7",
+    "url": "./static/js/runtime~main.b2522864.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d93a72f982f70f44f07d853963721dd2.js"
+  "./precache-manifest.11a10de1162f776c50ce28d96c8bae96.js"
 );
 
 self.addEventListener('message', (event) => {
